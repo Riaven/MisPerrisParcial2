5742a4e4ee57181cfdbcf8e44bc8481ec8fe4382
Jocelyn <jogubaez@hotmail.com>
2018-10-23 19:13:03 -0300

    Eliminar y modificar completos

--- misperris/apps/rescatado/urls.py
+++ misperris/apps/rescatado/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from apps.rescatado.views import index, rescatado_view, rescatado_list
+from apps.rescatado.views import index, rescatado_view, rescatado_list, rescatado_edit, rescatado_delete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^nuevo$', rescatado_view, name='rescatado_crear'),
     url(r'^listar$', rescatado_list, name='rescatado_listar'),
+    url(r'^editar/(?P<id_rescatado>\d+)/$', rescatado_edit, name='rescatado_editar'),
+    url(r'^eliminar/(?P<id_rescatado>\d+)/$', rescatado_delete, name='rescatado_eliminar'),
 ]
--- misperris/apps/rescatado/views.py
+++ misperris/apps/rescatado/views.py
@@ -36,3 +36,22 @@ def rescatado_list(request):
     rescatado = Rescatado.objects.all()
     contexto = {'rescatados':rescatado}
     return render(request, 'rescatado/rescatado_list.html', contexto)
+
+
+def rescatado_edit(request, id_rescatado):
+    rescatado = Rescatado.objects.get(id=id_rescatado)
+    if request.method == 'GET':
+        form = RescatadoForm(instance=rescatado)
+    else:
+        form = RescatadoForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('rescatado_listar')
+    return render(request,'rescatado/rescatado_form.html',{'form':form})
+
+def rescatado_delete(request, id_rescatado):
+    rescatado = Rescatado.objects.get(id=id_rescatado)
+    if request.method == 'POST':
+        rescatado.delete()
+        return redirect('rescatado_listar')
+    return render(request,'rescatado/rescatado_delete.html', {'rescatado':rescatado})
