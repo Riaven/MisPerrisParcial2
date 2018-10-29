b7f28a889baadb7791aff40e1f2802fe54a7ccc9
Jocelyn <jogubaez@hotmail.com>
2018-10-29 00:57:40 -0300

    restablecer completo - sin glamour

--- misperris/misperris/settings.py
+++ misperris/misperris/settings.py
@@ -131,9 +131,10 @@ LOGIN_REDIRECT_URL = reverse_lazy('rescatado_listar')
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smp.gmail.com'
-EMAIL_PORT = 25
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'librerialmg@gmail.com'
 EMAIL_HOST_PASSWORD = 'lapizlopez'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_PORT = 587
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
